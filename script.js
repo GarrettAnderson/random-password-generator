479d4b2340d4fbd0ac0f51585d77e4960819b783
Garrett <garrettleegrahamanderson@gmail.com>
2022-11-09 17:34:44 -0500

    added prompts for the number of characters along with what type of characters user will include in the password

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 // Assignment code here
 
-const passwordChars= "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()"
+// const passwordChars= "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()"
+
+let lengthOfPassword
+let uppercaseChars
+let lowercaseChars
+let specialChars 
+let numberChars 
 
 // create a object with key of number and values, another key of lower case alphabet, another key of uppercase alphabet, key of special characters
-
 const chars = {
   num: "1234567890",
   alphaLower: "abcdefghijklmnopqrstuvwxyz",
@@ -14,7 +19,10 @@ const chars = {
 // math.floor(math.random() * string.length) -> give the index of the string
 
 function generatePassword() {
-  // 
+
+  
+
+
 }
 
 // Get references to the #generate element
@@ -22,6 +30,18 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+
+  // Provide input for user to indicate how long password will be
+  lengthOfPassword = Number(prompt("Choose your password length between 8 and 128 characters."))
+  // prompt user on whether or not to include uppercase letters
+  uppercaseChars = prompt("Do you want to include uppercase letters?")
+  // prompt user on whether or not to include lowercase letters
+  lowercaseChars = prompt("Do you want to include lowercase letters?")
+  // promt user on whether or not to include special characters
+  specialChars = prompt("Do you want to include special characters?")
+  // prompt user on whether or not to include numbers
+  numberChars = prompt("Do you want to include numbers?")
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -31,4 +51,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-gi
