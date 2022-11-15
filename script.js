230e802b1eb986ccfea83579761fdfec17c3b3c5
Garrett <garrettleegrahamanderson@gmail.com>
2022-11-15 12:56:25 -0500

    added a while loop to ensure a password length is input into the prompt.

--- script.js
+++ script.js
@@ -19,31 +19,37 @@ const chars = {
 
 // math.floor(math.random() * string.length) -> give the index of the string
 
-function generatePassword() {
-  // Provide input for user to indicate how long password will be
-  lengthOfPassword = Number(prompt("Choose your password length between 8 and 128 characters."))
-  // validate that lengthOfPassword is between 8 and 128.
-  // if not prompt user to include a number between 8 and 128
-  if (!lengthOfPassword) {
-    alert("Need to input a number between 8 and 128.")
-    lengthOfPassword = Number(prompt("Choose your password length between 8 and 128 characters."))
-  } else if (lengthOfPassword < 8 || lengthOfPassword > 128) {
-    // if length of is less than 8 or more than 128 default to 12
-    // lengthOfPassword = Number(prompt("Choose your password length between 8 and 128 characters."))
-    lengthOfPassword = 12
-  } 
-    // prompt user on whether or not to include uppercase letters
-    uppercaseChars = prompt("Do you want to include uppercase letters? Please answer yes or no")
-    // prompt user on whether or not to include lowercase letters
-    lowercaseChars = prompt("Do you want to include lowercase letters? Please answer yes or no")
-    // promt user on whether or not to include special characters
-    specialChars = prompt("Do you want to include special characters? Please answer yes or no")
-    // prompt user on whether or not to include numbers
-    numberChars = prompt("Do you want to include numbers? Please answer yes or no")
 
-    // if user writes yes for uppercase letters, add uppercase letters from object to new string
+function promptCriteria() {
+
+    // Provide input for user to indicate how long password will be
+    lengthOfPassword = Number(prompt("Choose your password length between 8 and 128 characters."))
+    // validate that lengthOfPassword is between 8 and 128.
+    // if not prompt user to include a number between 8 and 128
+    
+    while (lengthOfPassword < 8 || lengthOfPassword > 128) {
+      alert("Need to input a number between 8 and 128.")
+      lengthOfPassword = Number(prompt("Choose your password length between 8 and 128 characters."))
+    }
+
+      // prompt user on whether or not to include uppercase letters
+      uppercaseChars = prompt("Do you want to include uppercase letters? Please answer yes or no")
+      // prompt user on whether or not to include lowercase letters
+      lowercaseChars = prompt("Do you want to include lowercase letters? Please answer yes or no")
+      // promt user on whether or not to include special characters
+      specialChars = prompt("Do you want to include special characters? Please answer yes or no")
+      // prompt user on whether or not to include numbers
+      numberChars = prompt("Do you want to include numbers? Please answer yes or no")
+
+}
+
+function validatePrompts() {
+      // if user writes yes for uppercase letters, add uppercase letters from object to new string
       // if user writes no, move to next prompt, 
     // if user writes yes for lowercase letters, add lowercase letters from object to same string as above
+
+    //use the confirm method to see if condition is met
+
     if (uppercaseChars == "yes" && lowercaseChars == "yes" && specialChars == "yes" && numberChars == "yes") {
       stringToRandomize = chars["alphaUpper"]+ chars["alphaLower"] + chars["specialChars"] + chars["num"]
       console.log(stringToRandomize, lengthOfPassword)
@@ -63,6 +69,13 @@ function generatePassword() {
       stringToRandomize = chars["num"]
       console.log(stringToRandomize)
     }
+}
+
+
+function generatePassword() {
+
+    promptCriteria()
+    validatePrompts()
     
     let randomizedString = ""
     for (let i = 0; i < lengthOfPassword; i++) {
